e0b901ef4f54ecde35206db7cdba868967b43258
rfedev <richardfelgin@gmail.com>
2015-11-12 08:18:25 +0000

    Commit

---
+++ app/src/main/java/com/sims/rfe/world/Objects/Tools/Saw.java
@@ -0,0 +1,10 @@
+package com.sims.rfe.world.Objects.Tools;
+
+/**
+ * Created by richa on 11/11/2015.
+ */
+public class Saw extends Tool {
+
+    private int durabitly;
+    private int quality;
+}
--- app/src/main/java/com/sims/rfe/world/Objects/WorldObject.java
+++ app/src/main/java/com/sims/rfe/world/Objects/WorldObject.java
@@ -1,12 +1,28 @@
 package com.sims.rfe.world.Objects;
 
-/**
- * Any object that can exist in the world.
- */
-public interface WorldObject {
+import com.sims.rfe.world.Objects.IWorldObject;
 
-    public String getName();
-    public int getId();
-    public boolean interact();
+/**
+ * Created by richa on 11/11/2015.
+ */
+public class WorldObject implements IWorldObject{
+
+    private String name;
+    private int id;
+
+    public WorldObject(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
 
 }
